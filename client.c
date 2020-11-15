8d70ac6c966dd9d8951089450414acc850364a2d
nrrnie <72966907+nrrnie@users.noreply.github.com>
2020-11-16 00:25:49 +0600

    Add files via upload

---
+++ client.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+
+#include <sys/socket.h>
+#include <sys/types.h>
+
+#include <netdb.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+int main (int argc, char const *argv[])
+{
+	int 				port, n, sfd;
+	struct  hostent 	*phe;
+	struct 	sockaddr_in sin;
+	time_t 				t;
+	
+	if (argc != 3)
+	{
+		fprintf(stderr, "Too few parameters\n");
+		exit (1);
+	}
+	
+	if (!(phe = gethostbyname(argv[1])))
+	{
+		fprintf(stderr, "Bad host name: %s\n", argv[1]);
+		exit (1);
+	}
+	
+	if (sscanf(argv[2], "%d%n", &port, &n) != 1 
+		|| argv[2][n] || port <= 0 || port > 65535)
+	{
+		fprintf(stderr, "Bad port number: %s\n", argv[2]);
+		exit (1);
+	}
+	
+	if (sfd = socket(PF_INET, SOCK_STREAM, 0) < 0)
+	{
+		perror("socket");
+		exit (1);
+	}
+	
+	sin.sin_family 	= AF_INET;
+	sin.sin_port	= htons(port);
+	
+	if (connect(sfd, (struct sockaddr*) &sin, sizeof(sin)) < 0)
+	{
+		perror("connect");
+		exit (1);
+	}
+	
+	t = time (0);
+	t = htonl(t);
+	
+	if (read(sfd, &t, sizeof(t)) != sizeof(t))
+	{
+		fprintf(stderr, "read failed\n");
+	} else {
+		printf("Server time: %s", ctime(&t));
+	}
+	
+	close(sfd);
+	return 0;
+}
---
+++ server.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/wait.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <time.h>
+
+int main (int argc, char const *argv[])
+{
+	int 				lfd, n, port, afd, user, sopt, pid;
+	struct sockaddr_in 	baddr, aaddr;
+	time_t 				ct, st;
+	
+	if (argc != 2)
+	{
+		fprintf(stderr, "Too few parameters\n");
+		exit (1);
+	}
+	if ((lfd = socket(PF_INET, SOCK_STREAM, 0)) < 0)
+	{
+		perror("socket");
+		exit (1);
+	}
+	if (setsockopt(lfd, SOL_SOCKET, SO_REUSEADDR, &sopt, sizeof(sopt)) < 0)
+	{
+		perror("setsockopt");
+		exit (1);
+	}
+	
+	baddr.sin_family 		= AF_INET;
+	baddr.sin_port 			= htons(port);
+	baddr.sin_addr.s_addr 	= INADDR_ANY;
+	
+	if (bind())
+	{
+		perror("bind");
+		exit (1);
+	}
+	if (listen(lfd, 5) < 0)
+	{
+		perror("listen");
+		exit (1);
+	}
+	
+	
+	while (1)
+	{
+		while (waitpid(-1, NULL, WNOHANG) > 0);
+		user = sizeof(aaddr);
+		if ((afd = accept(lfd, (struct sockaddr*) &aaddr, &user)) < 0)
+		{
+			perror("accept");
+			exit (1);
+		}
+		
+		if ((pid = fork()) < 0)
+		{
+			perror("fork");
+			exit (1);
+		} else if {
+			close(lfd);
+			printf("%d: Connection from %s: %d accepted.\n", getpid(),
+					inet_ntoa(aaddr.sin_addr), ntohs(aaddr.sin_port));
+			if (read(afd, &ct, sizeof(ct)) != sizeof(st))
+			{
+				fprintf(stderr, "read error");
+			} else {
+				ct = ntohl(ct);
+				st = time  (0);
+				printf("%d: Client time %lu, server time %lu\n", getpid(), ct, st);
+				st = htonl(st);
+				write(afd, &st, sizeof(st));
+			}
+			close(afd);
+			_exit (0);
+		}
+		close(afd);
+	}
+	return 0;
+}
